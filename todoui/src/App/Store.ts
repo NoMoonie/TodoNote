f173de78c0e1cf22587db34179dbf0e5c69a7fc3
NoMoonie <46845957+NoMoonie@users.noreply.github.com>
2021-11-11 16:44:18 +0100

    added selectedTodo and editor reducers

--- todoui/src/App/Store.ts
+++ todoui/src/App/Store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/todoSlice";
+import selectedtodoReducer from "../features/selectedTodoSlice";
+import editorReducer from "../features/editorSlice";
 
 export const store = configureStore({
     reducer: {
         todos: todoReducer,
+        selectedTodo: selectedtodoReducer,
+        editor: editorReducer,
     },
 });
 
